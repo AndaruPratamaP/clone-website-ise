bcb6aeaa9b0ea970f1151811f241d47af95cd2a0
nathanaelpganata <nathanaelpg8@gmail.com>
2023-06-02 13:55:38 +0700

    fix: gts form adjustments

--- app/Http/Controllers/Pages/Dashboard/Icon/GtsRegistration.php
+++ app/Http/Controllers/Pages/Dashboard/Icon/GtsRegistration.php
@@ -3,11 +3,41 @@
 namespace App\Http\Controllers\Pages\Dashboard\Icon;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
+
 
 class GtsRegistration extends Component
 {
+
+    use WithFileUploads;
+
+    public $name;
+    public $email;
+    public $sumber_informasi;
+    public $bukti_ig;
+    public $agree;
+
+
+    protected $rules = [
+        'name' => 'required|min:5|max:255',
+        'email' => 'required|email|string',
+        'sumber_informasi' => 'required|string',
+        'bukti_ig' => 'required|image:max:1024',
+        'agree' => 'required',
+    ];
+
     public function render()
     {
         return view('livewire.dashboard.my.icon.gts-registration')->layout('layouts.only-layout');
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submit()
+    {
+        $this->validate($this->rules);
     }
 }
--- resources/views/livewire/dashboard/my/icon/gts-registration.blade.php
+++ resources/views/livewire/dashboard/my/icon/gts-registration.blade.php
@@ -19,20 +19,23 @@
                                 <div class="relative z-0 w-full mb-[15px] lg:mb-[45px] group">
                                     <label for="name" class="block leading-normal text-[12px] lg:text-[20px] font-medium text-white">Nama Lengkap <span class="text-[#FF1F00]">*</span>
                                     </label>
-                                    <input type="text" name="floating_first_name" id="floating_first_name" class="block py-0 px-0 w-full text-[11px] lg:text-[18px] font-medium text-[#7C7C7C] bg-transparent border-0 border-b border-white appearance-none dark:text-white dark:border-white dark:focus:border-[#66C1A7] focus:outline-none focus:ring-0 peer" placeholder="Masukkan nama lengkap kamu" required />
+                                    <input type="text" name="name" id="name" wire:model="name" class="block py-0 px-0 w-full text-[11px] lg:text-[18px] font-medium text-[#7C7C7C] bg-transparent border-0 border-b border-white appearance-none dark:text-white dark:border-white dark:focus:border-[#66C1A7] focus:outline-none focus:ring-0 peer" placeholder="Masukkan nama lengkap kamu" required />
+                                    @error('name')
+                                        <span class="text-red-500 text-sm font-thin">{{ $message }}</span>
+                                    @enderror
                                 </div>
                                 <div class="relative z-0 w-full mb-[15px] lg:mb-[45px] group">
                                     <label for="email" class="block leading-normal text-[12px] lg:text-[20px] font-medium text-white">Email <span class="text-[#FF1F00]">*</span>
                                     </label>
-                                    <input type="email" name="floating_first_name" id="floating_first_name" class="block py-0 px-0 w-full text-[11px] lg:text-[18px] font-medium text-[#7C7C7C] bg-transparent border-0 border-b border-white appearance-none dark:text-white dark:border-white dark:focus:border-[#66C1A7] focus:outline-none focus:ring-0 peer" placeholder="Masukkan email aktif kamu" required />
+                                    <input type="email" name="email" id="email" wire:model="email" class="block py-0 px-0 w-full text-[11px] lg:text-[18px] font-medium text-[#7C7C7C] bg-transparent border-0 border-b border-white appearance-none dark:text-white dark:border-white dark:focus:border-[#66C1A7] focus:outline-none focus:ring-0 peer" placeholder="Masukkan email aktif kamu" required />
                                     @error('email')
                                         <span class="text-red-500 text-sm font-thin">{{ $message }}</span>
                                     @enderror
                                 </div>
                                 <div class="relative z-0 w-full mb-[15px] lg:mb-[45px] group">
-                                    <label for="email" class="block leading-normal text-[12px] lg:text-[20px] font-medium text-white">Sumber Informasi <span class="text-[#FF1F00]">*</span>
+                                    <label for="sumber_informasi" class="block leading-normal text-[12px] lg:text-[20px] font-medium text-white">Sumber Informasi <span class="text-[#FF1F00]">*</span>
                                     </label>
-                                    <select id="sumber_informasi" class="block py-0 px-0 w-full text-[11px] lg:text-[18px] font-medium text-[#7C7C7C] bg-transparent border-0 border-b border-white appearance-none dark:border-white dark:focus:border-[#66C1A7] focus:outline-none focus:ring-0  peer">
+                                    <select id="sumber_informasi" name="sumber_informasi" wire:model="sumber_informasi" class="block py-0 px-0 w-full text-[11px] lg:text-[18px] font-medium text-[#7C7C7C] bg-transparent border-0 border-b border-white appearance-none dark:border-white dark:focus:border-[#66C1A7] focus:outline-none focus:ring-0  peer">
                                         <option selected class="bg-[#1C1C1C]">Dari mana kamu mendapat informasi ini?</option>
                                         <option value="teman" class="bg-[#1C1C1C]">Teman</option>
                                         <option value="sosial_media" class="bg-[#1C1C1C]">Sosial Media</option>
@@ -41,14 +44,20 @@
                                         <span class="text-red-500 text-sm font-thin">{{ $message }}</span>
                                     @enderror
                                 </div>
-                                <div class="relative z-0 w-full mb-6 group">
-                                    <label for="text" class="block leading-normal text-[12px] lg:text-[20px] font-medium text-white">Bukti Upload Poster di IG Story <span class="text-[#FF1F00]">*</span>
+                                <div class="relative z-0 w-full mb-6 group cursor-pointer">
+                                    <label for="bukti_ig" class="block leading-normal text-[12px] lg:text-[20px] font-medium text-white">
+                                        Bukti Upload Poster di IG Story <span class="text-[#FF1F00]">*</span>
                                     </label>
-                                    <label class="mt-[13px] w-max px-2 py-[4px] flex flex-col items-center bg-[#1C1C1C] text-blue rounded-[6px] shadow-lg  border border-[#66C1A7] cursor-pointer hover:bg-blue hover:text-white">
-                                        <div class="flex flex-row items-center gap-[8px] hover:opacity-70 ">
-                                            <x-heroicon-s-cloud-arrow-up class="text-slate-100 w-4 md:w-8 h-4 md:h-8"/>
-                                            <span class="text-white font-Inter font-semibold text-xs md:text-[14px] whitespace-nowrap">Upload File</span>
-                                            <input type='file' class="hidden" id="bukti_ig" />
+                                    <label class="mt-[13px] w-max px-2 py-1 lg:py-[4px] flex flex-col items-center bg-[#1C1C1C] text-blue rounded-[6px] shadow-lg border border-[#66C1A7] cursor-pointer">
+                                        <div class="flex flex-row items-center gap-1 hover:brightness-75">
+                                            <x-heroicon-s-cloud-arrow-up class="text-slate-100 w-4 lg:w-8 h-4 lg:h-8"/>
+                                            <input type='file' class="block w-44 lg:w-56 text-xs lg:text-sm text-gray-500
+                                            file:mr-0 lg:file:mr-4 file:py-1 lg:file:py-2
+                                            file:rounded-md file:border-0
+                                            file:text-xs lg:file:text-sm file:font-semibold
+                                            file:bg-transparent file:text-white
+                                            file:cursor-pointer cursor-pointer
+                                          " wire:model="bukti_ig" id="bukti_ig" name="bukti_ig" />
                                         </div>
                                     </label>
                                     @error('bukti_ig')
@@ -56,9 +65,9 @@
                                     @enderror
                                 </div>
                                 <div class="lg:ml-[45.5px] flex items-center flex-row mt-[27px] mb-[20px] lg:mb-[39px] space-x-4 font-medium text-[11px] lg:text-[17px]">
-                                    <input wire:model.defer="agree" type="checkbox" id="setuju-kebijakan" name="setuju-kebijakan"
+                                    <input wire:model.defer="agree" type="checkbox" id="agree" name="agree"
                                         class="rounded-md cursor-pointer checked:bg-purple-200 checked:ring-purple-200 focus:ring-purple-200">
-                                    <label class="text-white" for="setuju-kebijakan"> Saya setuju dengan kebijakan privasi serta <a
+                                    <label class="text-white" for="agree"> Saya setuju dengan kebijakan privasi serta <a
                                             href="" class="text-[#66C1A7]" target="_blank">syarat dan
                                             ketentuan</a> yang berlaku</label>
                                     @error('agree')
@@ -76,10 +85,10 @@
                         </p>
                         <div class="flex flex-row justify-center gap-[16px] mt-6 sm:mt-[39px]">
                             <div>
-                                <a href="#" type="submit" class="items-center flex justify-center text-white bg-[##1C1C1C] font-semibold ring-2 ring-[#66C1A7] ring-inset font-Inter rounded-md text-[12px] lg:text-[16px] w-28 sm:w-[146.5px] lg:w-[387.5px] h-[42px] lg:h-[48px] px-5 py-2.5 text-center  dark:focus:ring-yellow-500 hover:opacity-80">Tutup</a>
+                                <a href="#" class="items-center flex justify-center text-white bg-[##1C1C1C] font-semibold ring-2 ring-[#66C1A7] ring-inset font-Inter rounded-md text-[12px] lg:text-[16px] w-28 sm:w-[146.5px] lg:w-[387.5px] h-[42px] lg:h-[48px] px-5 py-2.5 text-center  dark:focus:ring-yellow-500 hover:opacity-80">Tutup</a>
                             </div>
                             <div>
-                                <a href="#" type="submit" class="items-center flex justify-center text-white bg-gradient-to-r from-[#006C56] to-[#AFD196] hover:bg-blue-800 focus:ring-4 focus:outline-none font-semibold font-Inter rounded-md text-[12px] lg:text-[16px] w-28 sm:w-[146.5px] lg:w-[387.5px] h-[42px] lg:h-[48px] px-[12px] lg:px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-200 dark:focus:ring-[#66C1A7] hover:opacity-80">Masuk ke dashboard</a>
+                                <a href="#" class="items-center flex justify-center text-white bg-gradient-to-r from-[#006C56] to-[#AFD196] hover:bg-blue-800 focus:ring-4 focus:outline-none font-semibold font-Inter rounded-md text-[12px] lg:text-[16px] w-28 sm:w-[146.5px] lg:w-[387.5px] h-[42px] lg:h-[48px] px-[12px] lg:px-5 py-2.5 text-center dark:bg-yellow-600 dark:hover:bg-yellow-200 dark:focus:ring-[#66C1A7] hover:opacity-80">Masuk ke dashboard</a>
                             </div>
                         </div>
                     </div>
@@ -119,7 +128,3 @@
 
 
     </div>
-
-
-
-
