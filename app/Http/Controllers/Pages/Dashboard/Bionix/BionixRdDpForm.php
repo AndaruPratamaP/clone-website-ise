fbc4b62a054185da294a39a67c9a12014beeb582
Titus Patrick <100205500+drkpega@users.noreply.github.com>
2023-07-17 13:54:13 +0700

    Is 102  fe form roadshow dp bionix (#108)
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * fix: button size
    
    ---------
    
    Co-authored-by: nathanaelpganata <nathanaelpg8@gmail.com>

--- app/Http/Controllers/Pages/Dashboard/Bionix/BionixRdDpForm.php
+++ app/Http/Controllers/Pages/Dashboard/Bionix/BionixRdDpForm.php
@@ -3,11 +3,46 @@
 namespace App\Http\Controllers\Pages\Dashboard\Bionix;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class BionixRdDpForm extends Component
 {
     public function render()
     {
         return view('livewire.dashboard.my.bionix.bionix-rd-dp-form')->layout('layouts.only-layout');
+    }
+    use WithFileUploads;
+
+    public string $team_name = '';
+    public string $school;
+    public string $leader_name = '';
+    public string $rek_name = '';
+    public string $nominal_dp = '';
+    public string $promo_code = '';
+    public string $payment_method;
+    public $payment_proof;
+
+    public array $payment_methods = ['BCA - 90281741024', 'BNI - 0811111111'];
+
+    protected $rules = [
+      'team_name' => 'required|max:255',
+      'school' => 'required',
+      'leader_name' => 'required|max:255',
+      'rek_name' => 'required|max:255',
+      'nominal_dp' => 'required|max:255',
+      'promo_code' => 'required|max:255',
+      'payment_method' => 'required',
+      'payment_proof' => 'required|image|max:1024',
+    ];
+
+    public function updated($propertyName)
+    {
+      $this->validateOnly($propertyName);
+    }
+
+    public function submit()
+    {
+      $this->validate($this->rules);
+      dd($this);
     }
 }
