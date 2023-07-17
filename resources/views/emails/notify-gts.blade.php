3d6fad287323df5100b58bbae408d153bae177ef
nathanaelpganata <nathanaelpg8@gmail.com>
2023-07-17 13:17:17 +0700

    feat: add gts notif email template

---
+++ resources/views/emails/notify-gts.blade.php
@@ -0,0 +1,382 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html charset=UTF-8" />
+
+        <style>
+            .body {
+                color: white;
+            }
+            .container {
+                background-color: #191a1e;
+                width: 300px;
+                height: max-content;
+                font-family: "Open Sans", sans-serif;
+                padding: 1rem 2rem;
+                margin: auto;
+                color: white;
+                border-radius: 16px 0px 16px 0px;
+                justify-items: center;
+                text-align: center;
+                font-family: Poppins, sans-serif;
+            }
+
+            .title {
+                font-weight: 700;
+                font-size: 24px;
+                color: white;
+            }
+
+            .desc-text {
+                font-weight: 500;
+                font-size: 18px;
+                text-align: justify;
+            }
+
+            .greetings-1-text {
+                margin: 0;
+                text-align: left;
+                font-weight: 500;
+                font-size: 18px;
+            }
+
+            .greetings-2-text {
+                margin: 0;
+                text-align: left;
+                font-weight: 700;
+                font-size: 18px;
+            }
+
+            .greetings-text:second-child {
+                font-weight: 700;
+                font-size: 18px;
+            }
+
+            a {
+                color: inherit;
+                text-decoration: none;
+            }
+
+            .verify-btn {
+                background: linear-gradient(to right, #006c56, #afd196);
+                border-radius: 24px;
+                width: 200px;
+                text-align: center;
+                font-size: 16px;
+                font-weight: 600;
+                padding: 16px 40px;
+                margin: 24px auto;
+            }
+
+            .verify-btn:hover {
+                opacity: 0.8;
+            }
+
+            .contactus-1-text {
+                margin: 10px 0px 0px 0px;
+                font-weight: 500;
+                font-size: 18px;
+                text-align: center;
+                color: #b5b3bc;
+            }
+
+            .contactus-2-text {
+                margin: 2px 0px 0px 0px;
+                font-weight: 500;
+                font-size: 18px;
+                text-align: center;
+                color: #b5b3bc;
+            }
+
+            .contactus-btn {
+                background: #3d3e41;
+                border-radius: 10px;
+                width: 150px;
+                font-size: 16px;
+                padding: 10px 6px;
+                font-weight: 600;
+                text-align: center;
+                margin: 20px auto;
+                gap: 10px;
+            }
+
+            .contactus-btn:hover {
+                opacity: 0.8;
+            }
+
+            .phone-icon {
+                width: 20px;
+                height: 20px;
+            }
+
+            hr {
+                width: 100%;
+                border: 1px solid #afd196;
+            }
+        </style>
+    </head>
+
+    <body>
+        <table class="container" cellpadding="0" cellspacing="0" border="0">
+            <tr>
+                <td class="title">[D-5] ISE! Grand Talkshow 2023</td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 20px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">Hi, Futurizen!</td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 10px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    We would like to express our heartfelt gratitude to each and
+                    every one of you for being a part of ISE! Grand Talkshow
+                    2023.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 10px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    We have an exciting interactive talk show lined up for you,
+                    featuring renowned speakers in their respective fields. It
+                    will cover a wide range of interesting topics, presented in
+                    a more relaxed and interactive atmosphere. In this year's
+                    grand talk show, we will delve into three captivating
+                    topics, namely:
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 15px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <hr />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 15px 0px"></p>
+                </td>
+            </tr>
+
+            <tr>
+                <td class="desc-text">
+                    <span style="font-weight: 600">Topic 1: </span>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    Are We Heading Towards The Future with No Job Vacancy for
+                    Humans?
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 5px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    <span style="font-weight: 600">Speaker 1: </span>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    Irzan Raditya (CEO & Co-Founder of Kata.ai)
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 30px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    <span style="font-weight: 600">Topic 2: </span>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    Paving Pathways to IT Career Opportunities in the Digital
+                    Era
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 5px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    <span style="font-weight: 600">Speaker 2: </span>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    Belladonna Paramayswari (Risk Specialist at PT. Bank Mandiri
+                    (Persero) Tbk.)
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 30px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    <span style="font-weight: 600">Topic 3: </span>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    Unleashing the Power of IT Implementation in Business
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 5px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    <span style="font-weight: 600">Speaker 3: </span>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    Sabrina Woro Anggraini (Co-Founder & Managing Director of
+                    Natuno Design Lab)
+                </td>
+            </tr>
+            <!-- <tr>
+                <td>
+                    <a
+                        class="verify-btn"
+                        href="{{ $app_url . '/verify/' . $token }}"
+                        >Verifikasi Email</a
+                    >
+                </td>
+            </tr> -->
+            <tr>
+                <td>
+                    <p style="margin: 40px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    Therefore, we would like to remind you that ISE! Grand
+                    Talkshow 2023 will be held on Saturday, 22nd July 2023 at
+                    10.00 AM (WIB). You’ll be able to join the talk show at the
+                    scheduled time by clicking the link provided in the next
+                    email, so turn on your notification and stay tuned!
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 20px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    Look forward to seeing you at the event! 👋🏻
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 20px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    If you have any questions, please contact us via WhatsApp
+                    Center (0281-3320-49750)
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 20px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text">
+                    For more information, visit our social media at :
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text" style="padding: 0px 0px 0px 10px">
+                    Instagram : @is_expo
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text" style="padding: 0px 0px 0px 10px">
+                    Twitter : @is_expo
+                </td>
+            </tr>
+            <tr>
+                <td class="desc-text" style="padding: 0px 0px 0px 10px">
+                    Line : @mfd0663i
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <p style="margin: 20px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td class="greetings-1-text">Best Regards,</td>
+            </tr>
+            <tr>
+                <td class="greetings-2-text">ISE! 2023</td>
+            </tr>
+            <tr>
+                <td class="greetings-1-text">One Aim, One Goal, One ISE!</td>
+            </tr>
+            <tr>
+                <td>
+                    <hr />
+                </td>
+            </tr>
+            <tr>
+                <td class="contactus-1-text">Ada pertanyaan?</td>
+            </tr>
+            <tr>
+                <td class="contactus-2-text">Silakan hubungi kami</td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 20px 0px"></p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a
+                        href="https://wa.me/+6281332049750"
+                        class="contactus-btn"
+                        target="blank"
+                    >
+                        Hubungi Kami</a
+                    >
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p style="margin: 10px 0px"></p>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
