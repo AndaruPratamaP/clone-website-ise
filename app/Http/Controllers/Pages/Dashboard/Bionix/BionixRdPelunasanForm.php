18631f4fc6f10e58b5331346f490c12a384395de
Jhoni Ananta Sitepu <93531492+jhoniananta@users.noreply.github.com>
2023-07-20 19:05:53 +0700

    feat:add pelunasan-bionix (#110)
    * feat:add pelunasan-bionix
    
    * add feat: add bionix-pelunasan

--- app/Http/Controllers/Pages/Dashboard/Bionix/BionixRdPelunasanForm.php
+++ app/Http/Controllers/Pages/Dashboard/Bionix/BionixRdPelunasanForm.php
@@ -3,11 +3,30 @@
 namespace App\Http\Controllers\Pages\Dashboard\Bionix;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class BionixRdPelunasanForm extends Component
 {
-    public function render()
-    {
-        return view('livewire.dashboard.my.bionix.bionix-rd-pelunasan-form')->layout('layouts.only-layout');
-    }
+  use WithFileUploads;
+  public string $team_name = '';
+  public $bukti;
+
+  protected $rules = [
+    'team_name' => 'required|max:255',
+    'bukti' => 'required|image|max:1024',
+  ];
+  public function render()
+  {
+    return view('livewire.dashboard.my.bionix.bionix-rd-pelunasan-form')->layout('layouts.only-layout');
+  }
+  public function updated($propertyName)
+  {
+    $this->validateOnly($propertyName);
+  }
+
+  public function submit()
+  {
+    $this->validate($this->rules);
+    dd($this);
+  }
 }
